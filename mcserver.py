d516385277a359dd322e5d9beb7e03fd3f0118a6
aekblaw <aekblaw@users.noreply.github.com>
2015-11-30 22:13:47 -0500

    hw3 adds input dates, weekly charting view

--- mcserver.py
+++ mcserver.py
@@ -1,4 +1,5 @@
-import ConfigParser, logging, datetime, os
+import ConfigParser, logging, datetime, os, collections
+
 
 from flask import Flask, render_template, request
 
@@ -28,13 +29,46 @@ def home():
 @app.route("/search",methods=['POST'])
 def search_results():
     keywords = request.form['keywords']
-    now = datetime.datetime.now()
+    #now = datetime.datetime.now()
+    
+    #Could make these more elegant by grabbing a tuple of all three at once, rather than separately. Could include better form troubleshooting (like only allowing integers)
+
+    startYear = int(request.form['year1'])
+    print request.form['year1']
+    print type(request.form['year1'])
+    startMonth = int(request.form['month1'])
+    startDay = int(request.form['day1'])
+
+    endYear = int(request.form['year2'])
+    endMonth = int(request.form['month2'])
+    endDay = int(request.form['day2'])
+
+    startDate = datetime.date(startYear, startMonth, startDay)
+    endDate = datetime.date(endYear, endMonth, endDay)
+
+
     results = mc.sentenceCount(keywords,
-        solr_filter=[mc.publish_date_query( datetime.date( 2015, 1, 1), 
-                                            datetime.date( now.year, now.month, now.day) ),
-                     'media_sets_id:1' ])
+        solr_filter=[mc.publish_date_query( startDate, 
+                                            endDate),
+                     'media_sets_id:1' ],split = True, split_start_date = str(startDate), split_end_date = str(endDate))
+
+
+    #sorting functionality below adapted with help from Jasmin and Penny's code
+
+    dataPoints = results['split']
+
+
+    sorting = collections.OrderedDict(sorted(dataPoints.items()))
+    
+    weeks = [key[:10] for key in sorting.keys()[:-3]]
+
+    dataPointsSorted = sorting.values()[:-3]
+
+    print dataPointsSorted
+    print weeks
+    
     return render_template("search-results.html", 
-        keywords=keywords, sentenceCount=results['count'] )
+        keywords=keywords, sentenceCount=results['count'], data=dataPointsSorted, weeks=weeks )
 
 if __name__ == "__main__":
     app.debug = True
